c62f446a6c2664e956fc65fb2c519fe49f40478b
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-05-29 14:40:31 +0530

    updated

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Dholera Smart City">
-  <title>Dholera Smart City</title>
+  <title>Kavyaprop.com</title>
   
   <style>
     body {
